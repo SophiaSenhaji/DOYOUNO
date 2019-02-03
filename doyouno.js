e323e0059d704d2a5812bf7fd28efcd1c149dc77
Sophia <sophia.senhaji@gmail.com>
2019-02-03 17:37:45 +0100

    replace for with while + fix indentation

--- doyouno.js
+++ doyouno.js
@@ -7,15 +7,15 @@ const reverseString = text => {
   }
   
 const unhash = number => {
-let text = '';
-let value = number;
-const letters = "acdegilmnoprstuw";
-for (let i = 0; i < 8 && value > 7; i++) {
-    text += letters[value%37];
-    value = Math.floor(value/37);
-}
+    let text = '';
+    let value = number;
+    const letters = "acdegilmnoprstuw";
+    while (value > 7) {
+        text += letters[value%37];
+        value = Math.floor(value/37);
+    }
 
-return reverseString(text);
+    return reverseString(text);
 }
   
 unhash(24682779393128);
