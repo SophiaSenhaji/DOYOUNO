ec43ec15d755a52716e18bb11e1115d8bd3b95f1
Sophia <sophia.senhaji@gmail.com>
2019-02-03 17:26:03 +0100

    add:  unhash fonction

---
+++ doyouno.js
@@ -0,0 +1,21 @@
+const reverseString = text => {
+    let result = ''
+    for (let i = text.length - 1; i > -1; i--) {
+      result += text[i];
+    }
+    return result;
+  }
+  
+const unhash = number => {
+let text = '';
+let value = number;
+const letters = "acdegilmnoprstuw";
+for (let i = 0; i < 8 && value > 1; i++) {
+    text += letters[value%37];
+    value = Math.floor(value/37);
+}
+
+return reverseString(text);
+}
+  
+unhash(24682779393128);
